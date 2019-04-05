75714d31509c437502ccdd7a7256ef380596704a
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-05 19:41:11 +0000

    Modify appcontroller

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -5,4 +5,31 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "saleshelper"
   end 
+  
+  get '/' do
+    erb :"index.html"
+  end 
+  
+  helpers do 
+    def logged_in?
+      !!session[:representative_id]
+    end
+  
+    def current_user 
+      Representative.find_by_id(session[:representative_id])
+    end
+    
+    def login(username, password)
+      rep = Representative.find_by(:username => username)
+      if rep && rep.authenticate(password) 
+        session[:username] = rep.username
+      else 
+        redirect to "/login" 
+      end 
+    end 
+    
+    def logout!
+      session.clear
+    end 
+      
 end 
