85051ace14f42212d05b966ea588348c8e028547
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-05 18:26:22 +0000

    first commit

---
+++ lib/sales/helper.rb
@@ -0,0 +1,8 @@
+require "sales/helper/version"
+
+module Sales
+  module Helper
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sales/helper/version.rb
@@ -0,0 +1,5 @@
+module Sales
+  module Helper
+    VERSION = "0.1.0"
+  end
+end
