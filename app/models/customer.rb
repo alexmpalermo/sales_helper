9d84ef547bf675c8d87bd87ab79358356ad168c0
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-06 00:29:36 +0000

    Add slugs

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,10 @@
+require_relative "../models/concerns/slugifiable.rb"
+
 class Customer < ActiveRecord::Base 
   belongs_to :representative 
   has_many :customer_items
   has_many :items, through: :customer_items
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+  
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,4 +1,9 @@
+require_relative "../models/concerns/slugifiable.rb"
+
 class Representative < ActiveRecord::Base 
   has_secure_password 
   has_many :customers 
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+  
 end 
