1f8ca5cd35bd9f16c0bc624d7d2e33092bff6443
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-06 00:23:00 +0000

    Add to controllers

--- app/controller/customers_controller.rb
+++ app/controller/customers_controller.rb
@@ -1,8 +1,26 @@
 class CustomersController < ApplicationController
   
   get '/customers/new' do 
-    erb :"customers/new.html"
+    erb :"/customers/new.html"
   end
   
+  get '/customers/:slug' do 
+    erb :"/customers/show.html"
+  end 
   
+  get '/customers/:slug/edit' do 
+    erb :"/customers/edit.html"
+  end 
+  
+  post '/customers' do 
+    
+  end 
+  
+  patch '/customers/:slug' do 
+    
+  end 
+  
+  delete '/customers/:slug/delete' do 
+    
+  end 
 end
--- app/controller/representatives_controller.rb
+++ app/controller/representatives_controller.rb
@@ -1,8 +1,30 @@
 class RepresentativesController < ApplicationController
   
   get '/login' do 
-    erb :"representatives/login.html"
+    erb :"/representatives/login.html"
   end
+  
+  get '/signup' do
+    erb :"/representatives/signup.html"
+  end 
+  
+  get '/logout' do
+    
+  end 
+  
+  get '/representatives/:slug' do 
+    erb :"/representatives/show.html"
+  end 
+  
+  post '/login' do
+    
+    redirect to "/representatives/#{@rep.slug}"
+  end 
+  
+  post '/signup' do 
+    
+    redirect to "/representatives/#{@rep.slug}"
+  end 
   
   
 end
